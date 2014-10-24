c9b1d3874638ceaae7cf2578fc885547db88636c
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-10-23 20:19:32 -0700

    Simple list parsing

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,8 @@
 pub enum Sexpr {
   Nil,
   Int(i32),
-  Symbol(String)
+  Symbol(String),
+  Cons(Box<Sexpr>, Box<Sexpr>),
 }
 
 type ParseResult = Result<Sexpr, String>;
@@ -35,14 +36,28 @@ impl Identifiable for char {
 
 impl Parser {
   fn parse(&mut self) -> ParseResult {
+    self.consume_whitespace();
+    if self.eof() {
+      Ok(Nil)
+    } else {
+      self.do_parse()
+    }
+  }
+
+  fn do_parse(&mut self) -> ParseResult {
     match self.next_char() {
       c if c.is_digit() => self.parse_number(),
+      '(' => {
+        self.consume_char();
+        self.parse_list()
+      }
       _ => self.parse_symbol()
     }
   }
 
   fn parse_symbol(&mut self) -> ParseResult {
     let symbol = self.consume_while(|char| { char.is_valid_for_identifier() });
+    self.consume_whitespace();
 
     match symbol.as_slice() {
       "nil" => Ok(Nil),
@@ -52,9 +67,37 @@ impl Parser {
 
   fn parse_number(&mut self) -> ParseResult {
     let string = self.consume_while(|char| { char.is_digit() });
+    self.consume_whitespace();
     Ok(Int(from_str::<i32>(string.as_slice()).unwrap()))
   }
 
+  fn parse_list(&mut self) -> ParseResult {
+    let car = match self.parse() {
+        Ok(value) => value,
+        Err(e) => return Err(e)
+    };
+
+    let cdr = try!(self.parse_list_tail());
+
+    match self.next_char() {
+      ')' => {
+        self.consume_char();
+        Ok(Cons(box car, box cdr))
+      },
+      _ => Err("Expected )".to_string())
+    }
+  }
+
+  fn parse_list_tail(&mut self) -> ParseResult {
+    let car = match self.next_char() {
+      ')' => return Ok(Nil),
+       _  => try!(self.parse())
+    };
+
+    let cdr = try!(self.parse_list_tail());
+
+    Ok(Cons(box car, box cdr))
+  }
 
   /// Consume and discard zero or more whitespace characters.
   fn consume_whitespace(&mut self) {
@@ -84,7 +127,7 @@ impl Parser {
 
   /// Does the current input start with the given string?
   fn starts_with(&self, s: &str) -> bool {
-      self.input.as_slice().slice_from(self.pos).starts_with(s)
+      self.input.as_slice().slice_from(self.pos).trim().starts_with(s)
   }
 
   /// Return true if all input is consumed.
@@ -115,6 +158,21 @@ mod tests {
       let sexp = result.unwrap();
       match sexp {
           Symbol(x) => assert_eq!(x, "hello".to_string()),
+          _ => fail!("Parsed incorrectly, got {}", sexp)
+      };
+  }
+
+  #[test]
+  fn parses_lists() {
+      let result = parse("(1 2 3)".to_string());
+      assert!(result.is_ok(), "parse failed: {}", result);
+      let sexp = result.unwrap();
+      match sexp {
+          Cons(box Int(x), box Cons(box Int(y), box Cons(box Int(z), box Nil))) => {
+              assert_eq!(x, 1);
+              assert_eq!(y, 2);
+              assert_eq!(z, 3);
+          },
           _ => fail!("Parsed incorrectly, got {}", sexp)
       };
   }
